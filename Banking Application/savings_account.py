9c672a4924895117937393c35d2fc5ea04aed8d3
Wyatt Haak <wyatt.haak4@gmail.com>
2022-03-21 13:03:43 -0700

    Delete Banking Application directory

--- Banking Application/savings_account.py
+++
@@ -1,7 +0,0 @@
-from account import Account
-
-
-class SavingsAccount(Account):
-
-    def __init__(self, first_name, last_name, account_type, account_balance):
-        super().__init__(first_name, last_name, account_type, account_balance)
